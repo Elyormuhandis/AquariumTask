f3ea79df1ef7956f95970b7229b0ff4f78c4f15f
elyormuhandis <elyormuhandis@gmail.com>
2023-06-07 23:44:48 +0500

    warnings fix

--- AquariumApp.java
+++ AquariumApp.java
@@ -4,44 +4,44 @@ import java.util.stream.Collectors;
 public class AquariumApp {
 
     public static void main(String[] args) throws InterruptedException {
-        List<Fish> fishes = new ArrayList<>();
+        List<Fish> fishList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the our aquarium! \nEnter initial value of fishes:");
-        int quantityOfFishes = scanner.nextInt();
+        System.out.println("Welcome to the our aquarium! \nEnter initial value of fish:");
+        int quantityOfFish = scanner.nextInt();
         // This block for initialization
-        for (int i = 0; i < quantityOfFishes; i++) {
+        for (int i = 0; i < quantityOfFish; i++) {
             Fish fish = new Fish();
             fish.start();
-            fishes.add(fish);
+            fishList.add(fish);
         }
         // This block for simulate aquarium life
-        while (!fishes.isEmpty()) {
+        while (!fishList.isEmpty()) {
             Thread.sleep(1000); // Every second we can show information about aquarium
             Random random = new Random(); // This object used for randomize values
 
-            //This block for meeting of fishes, here fishes can spawning and maybe created new fish
-            int idx1=random.nextInt(fishes.size()-1);
-            if (random.nextInt(4) == 2 && !fishes.isEmpty()) {
-                int idx2=random.nextInt(fishes.size()-1);
+            //This block for meeting of fish, here fish spawning, maybe created new fish
+            int idx1=random.nextInt(fishList.size());
+            if (random.nextInt(4) == 2) {
+                int idx2=random.nextInt(fishList.size());
                 if (idx1!=idx2){
-                    Fish fish1 = fishes.get(idx1);
-                    Fish fish2 = fishes.get(idx2);
+                    Fish fish1 = fishList.get(idx1);
+                    Fish fish2 = fishList.get(idx2);
                     Fish newFish = fish1.spawning(fish2);
                     if (newFish!=null){
                         newFish.start();
-                        fishes.add(newFish);
+                        fishList.add(newFish);
                         System.out.println("New fish " + newFish);
                     }
                 }
             }
 
             //This block for checking fish is alive or die
-            fishes = fishes.stream().filter(fish -> fish.getAlive()).collect(Collectors.toList());
+            fishList = fishList.stream().filter(Fish::getAlive).collect(Collectors.toList());
 
             //This block for information about aquarium
-            System.out.println("The quantity of fishes: " + fishes.size());
-            System.out.println("The quantity of male fishes: " + fishes.stream().filter(fish -> fish.getSex().equals("MALE")).count());
-            System.out.println("The quantity of female fishes: " + fishes.stream().filter(fish -> fish.getSex().equals("FEMALE")).count());
+            System.out.println("The quantity of fish: " + fishList.size());
+            System.out.println("The quantity of male fish: " + fishList.stream().filter(fish -> fish.getSex().equals("MALE")).count());
+            System.out.println("The quantity of female fish: " + fishList.stream().filter(fish -> fish.getSex().equals("FEMALE")).count());
         }
         System.out.println("All the fish have died, the aquarium is empty.");
     }
--- Fish.java
+++ Fish.java
@@ -3,10 +3,10 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Fish extends Thread {
 
-    private String sex = ThreadLocalRandom.current().nextInt(1, 3) == 1 ? "MALE" : "FEMALE"; //Initial value of sex maybe MALE or FEMALE only
+    private final String sex = ThreadLocalRandom.current().nextInt(1, 3) == 1 ? "MALE" : "FEMALE"; //Initial value of sex maybe MALE or FEMALE only
 
-    private long birthTime = new Date().getTime(); //When object fish will be created, birthTime will initialize that time
-    private long lifespan = (long) ThreadLocalRandom.current().nextInt(10 * 1000, 60 * 1000); // The average lifespan of fishes from 1 to 60 second;
+    private final long birthTime = new Date().getTime(); //When object fish will be created, birthTime will initialize that time
+    private final long lifespan = ThreadLocalRandom.current().nextInt(10 * 1000, 60 * 1000); // The average lifespan of fish from 1 to 60 second;
     //Note! If you want to increase lifespan of fish, you need to change bound of random function
 
 
@@ -20,15 +20,13 @@ public class Fish extends Thread {
     //This is default function of Thread class.
     @Override
     public void run() {
-        try {
-            while (lifespan > new Date().getTime() - birthTime) {
-                Thread.sleep(1); // we can delete this partition, this is for testing
-            }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+
+        while (lifespan > new Date().getTime() - birthTime) {
+            continue;
         }
         System.out.println("The fish with id " + super.getId() + " was died!");
-        this.alive =false;
+        this.alive = false;
+
     }
 
     public Fish spawning(Fish fish) {
@@ -39,23 +37,16 @@ public class Fish extends Thread {
     }
 
 
-
     public String getSex() {
         return sex;
     }
 
-    public long getBirthTime() {
-        return birthTime;
-    }
 
-    public long getLifespan() {
-        return lifespan;
-    }
 
     @Override
     public String toString() {
         return "Fish{" +
-                "id=" + super.getId()+ ", " +
+                "id=" + super.getId() + ", " +
                 "sex='" + sex + '\'' +
                 ", birthTime=" + birthTime +
                 ", lifespan=" + lifespan +
